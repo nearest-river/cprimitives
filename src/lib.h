d1a5dc6a4f537bfa39874ff0c9523edb5fd99040
Nate River <trishiragolder69@outlook.com>
2024-10-31 08:42:26 +0530

    fixed defs for unsupported platdorms

--- src/lib.h
+++ src/lib.h
@@ -28,7 +28,9 @@ typedef char utf8char[4];
 typedef _Float16 f16;
 typedef float f32;
 typedef double f64;
+#ifdef __FLOAT128__
 typedef __float128 f128;
+#endif
 
 #ifndef __STDBOOL_H
 #define __bool_true_false_are_defined 1
